18a63ca892b01245d4d6af8d87f039fa34400921
Liscak Adam <1810653053@stud.fh-kufstein.ac.at>
2018-12-20 11:31:43 +0100

    aaufgabe4

--- src/A4.java
+++ src/A4.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class A4 {
+    public static void main(String[] args)
+    {
+        ArrayList<String> a= new ArrayList<>();
+        for (int i=0; i<5;i++)
+        {
+            a.add(JOptionPane.showInputDialog("Bitte namen eingem"));
+        }
+        for (String k: a  )
+        {
+            System.out.println(k);
+        }
+    }
 }
